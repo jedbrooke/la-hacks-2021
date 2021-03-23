4b4bb776fb03ba4eaf475f98f488de91619030bc
Matthew Atmadja <atmadja.matthew@gmail.com>
2021-03-22 22:22:27 -0700

    Update GUI.py

---
+++ GUI.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+window = tk.Tk()
+greeting = tk.Label(text="poop",
+                    fg="purple",
+                    bg="black",
+                    width=100,
+                    height=100)
+greeting.pack()
+
+
+window.mainloop()
