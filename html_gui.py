8b01d1b8e1fa523d5723c27dc2ee8845e8ecc23d
Jasper Edbrooke <jasperredrock@gmail.com>
2021-03-22 23:42:24 -0700

    add gui engine files

---
+++ html_gui.py
@@ -0,0 +1,16 @@
+import os
+from tkinter_engine.gui_engine import Form, Window, TagUtility, Button, Field
+
+def main():
+	windows = {
+
+	}
+
+	start_page = "main.html"
+	path = os.path.join("gui_pages",start_page)
+	w = Window(TagUtility.get_html(path),main=True,windows=windows)
+
+	w.start()
+
+if __name__ == '__main__':
+	main()
